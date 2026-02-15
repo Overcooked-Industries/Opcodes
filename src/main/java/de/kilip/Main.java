a71654844401e4750f920ee7d005f51e35b39447
Kilip1000 <206253641+Kilip1000@users.noreply.github.com>
2026-02-15 22:04:17 +0100

    some temp printing

--- src/main/java/de/kilip/Main.java
+++ src/main/java/de/kilip/Main.java
@@ -1,9 +1,10 @@
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import de.kilip.HttpDownloader;
 
-void main() {
+void main() throws IOException {
     String serverUrl = "https://piston-data.mojang.com/v1/objects/d3b86d840aa59a7512b6a028d40762cf6e492eeb/server.jar";
-
+    if(!HttpDownloader.downloadJar(serverUrl, "server.jar")) return;
+    var serverJar = new JarFile("server.jar");
+    serverJar.entries().asIterator().forEachRemaining(jarEntry -> IO.println(jarEntry.getName()));
 }
+
 
