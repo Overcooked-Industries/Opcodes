741a621d971d4cd1ae8515ccc13a54d33f3df21b
Kilip1000 <206253641+Kilip1000@users.noreply.github.com>
2026-02-16 15:29:19 +0100

    Print Opcodes?

--- src/main/java/de/kilip/JarDownloader.java
+++ src/main/java/de/kilip/JarDownloader.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 import java.util.jar.JarFile;
 
 public class JarDownloader {
-    public static boolean downloadJar(String url, String file_name) {
+    public static void downloadJar(String url, String file_name) {
         try (HttpClient client = HttpClient.newHttpClient()) {
             var request = HttpRequest.newBuilder().uri(URI.create(url)).build();
             HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
@@ -19,12 +19,10 @@ public class JarDownloader {
             if (response.statusCode() == 200) {
                 Path filePath = Paths.get(file_name);
                 Files.write(filePath, response.body());
-                return true;
             }
 
         } catch (Exception _) {
         }
-        return false;
     }
 
     public static JarFile downloadIfAbsent(String url, String file_name) throws IOException {
--- src/main/java/de/kilip/Main.java
+++ src/main/java/de/kilip/Main.java
@@ -1,13 +1,23 @@
 import de.kilip.JarDownloader;
+import de.kilip.OpCodes;
 
 void main() throws IOException {
     String url = "https://piston-data.mojang.com/v1/objects/ada715d3943e7584f04aca8ec44f5d3cd767353a/client.jar";
     String filename = "client.jar";
     var clientJar = JarDownloader.downloadIfAbsent(url, filename);
     clientJar.entries().asIterator().forEachRemaining(jarEntry -> {
-        if(!jarEntry.isDirectory() && jarEntry.getName().endsWith(".class")) {
+        try(InputStream inputStream = clientJar.getInputStream(jarEntry)) {
             String name = Arrays.stream(jarEntry.getName().split("/")).toList().getLast();
             IO.println(name);
+            byte[] bytes = inputStream.readAllBytes();
+            for(byte currentByte : bytes)
+            {
+                IO.println(OpCodes.opcodes[currentByte]);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     });
 }
